abddca4044798ea909f55a9abc272e91f82407a5
Vincent Chow <chowvk8@gmail.com>
2024-05-25 11:05:19 +0100

    Added comments

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# Import packages
 import pandas as pd
 from bs4 import BeautifulSoup
 
+# Open data file 
 with open("Untitled.html") as file:
+    # Parse html
     soup = BeautifulSoup(file, "html.parser")
+
+# read html as a dataframe
 df = pd.read_html(str(soup))
+
+# filter for first row (Containing dataframe)
 df = df[0]
