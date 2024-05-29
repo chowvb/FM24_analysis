c5422dcdd07231561667e1cd24d0f39e498da16b
Vincent Chow <chowvk8@gmail.com>
2024-05-29 19:02:33 +0100

    Added filter list and expiry script

---
+++ expiring_contracts.py
@@ -0,0 +1,49 @@
+# This script will review the players in the current squad whos contracts are due to expire at the end of the current and next season.
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# Read squad attributes html file
+with open("data/squad_data/attributes/player_attributes_23-24.html") as file:
+    # Parse html
+    soup = BeautifulSoup(file, "html.parser")
+# Read html file as a dataframe
+squad_attributes = pd.read_html(str(soup))
+# Filter df for the first row (Containing dataframe)
+squad_attributes = squad_attributes[0]
+
+# Read squad season stats html file 
+with open("data/squad_data/stats/player_stats_23-24.html") as file:
+    # Parse html
+    soup = BeautifulSoup(file, "html.parser")
+# Read html file as a dataframe
+squad_stats = pd.read_html(str(soup))
+# Filter df for the first row (Containing Dataframe)
+squad_stats = squad_stats[0]
+
+
+# Extracting players who's contracts are expiring.
+# Change the Contract Expiry date to only show the year of expiry
+squad_attributes["Expires"] = squad_attributes["Expires"].str[5:]
+
+# Get the most recent expiry year 
+expiry_year = squad_attributes["Expires"].iloc[0]
+
+# Filter dataframe for players whos contracts are expiring soon 
+expiring_players_df = squad_attributes.loc[squad_attributes["Expires"] == expiry_year]
+
+# Extract the player names 
+expiring_players = expiring_players_df["Name"].tolist()
+
+# Using the extracted players names who's contracts are soon to be expiring, filter the season stats for those players
+expiring_players_season_stats = squad_stats.loc[squad_stats["Name"].isin(expiring_players)]
+
+# Create a list of column names to filter for the most important stats in deciding to renew contracts
+filter_list = ["Name", "Best Pos", "Age", "Wage", "Transfer Value", "Apps", "Starts", "Mins", "Mins/Gm", "Won", "D", "Lost", "Gwin", "Av Rat", "Dist/90"]
+
+expiring_players_season_stats = expiring_players_season_stats[filter_list]
+
+position_list = squad_stats["Best Pos"].tolist()
+
+for i, position in enumerate(position_list):
+    new_position = position.replace("(", "").replace(")", "").replace(" ", "")
+    position_list[i] = new_position
---
+++ stats_by_position_functions.py
@@ -0,0 +1,22 @@
+# Here we will store lists for to filter dataframes for important stats for player positions
+
+# Goalkeeping Stats
+GK_stats = ["Name", "Age", "Apps", "Av Rat", "Clean Sheets", "Conc", "Con/90", "Gl Mst", "Mins", "Pas %", "Ps C/90", "Pens Faced", "Pens Saved", "Pens Save Ratio", "PoM", "Sv %", "xSv %", "Svh", "Svp", "Svt", "Tck", ]
+
+# Center Defencive Stats
+DC_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", "Gls", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red"]
+
+# Fullback/Wingback stats
+FB_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Cr A", "Cr C", "Cr C/A", "Dist/90", "DrbPg", "Fls", "Gl Mst", "Gls", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red"]
+
+# Defensive Midfield Stats
+DM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", "Gls", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red", "Dist/90", "CCC", "Ch C/90", "ShT/90", "Shot/90", "K Pas", "K Pas/90"]
+
+# Center Midfield Stats
+CM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", "Gls", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red", "Dist/90", "CCC", "Ch C/90", "ShT/90", "Shot/90", "K Pas", "K Pas/90"]
+
+# Attacking Midfield Stats (Incluing Wingers)
+AM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "xG", "Gls/90", "Mins/Gl", "Ast", "Asts/90", "CCC", "Ch C/90", "Cr A", "Cr C", "Cr C/A", "Dist/90", "FA", "K Pas", "K Pas/90", "Pas %", "Pas A", "Ps A/90","Ps C", "Ps C/90", "Shots", "ShT", "ShT/90", "Shot %"]
+
+# Striker Stats
+ST_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "xG", "Gls/90", "Mins/Gl", "Ast", "Asts/90", "CCC", "Ch C/90", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Dist/90", "FA", "K Pas", "K Pas/90", "Pas %", "Pas A", "Ps A/90","Ps C", "Ps C/90", "Shots", "Shot/90", "ShT","ShT/90", "Shot %"]
