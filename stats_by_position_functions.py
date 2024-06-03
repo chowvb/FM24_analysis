cf0d5b80b97ab3ca5883368705ed318e3b5c86ad
Vincent Chow <chowvk8@gmail.com>
2024-06-03 15:46:51 +0100

    Amended duplicate variables

--- stats_by_position_functions.py
+++ stats_by_position_functions.py
@@ -6,35 +6,35 @@ GK_stats = ["Name", "Age", "Apps", "Av Rat", "Clean Sheets", "Conc", "Con/90", "
             "Svt", "Tck", ]
 
 # Center Defencive Stats
-DC_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", "Gls", 
+DC_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Ast", "Fls", "Gl Mst", "Gls", 
             "Hdr %", "Hdrs", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", 
             "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red"]
 
 # Fullback/Wingback stats
 FB_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Cr A", "Cr C", "Cr C/A", 
-            "Dist/90", "DrbPg", "Fls", "Gl Mst", "Gls", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", 
+            "Dist/90", "DrbPg", "Fls", "Gl Mst", "Gls", "Hdr %", "Hdrs", "Hdrs A", "Hdrs W/90", "K Hdrs", 
             "Aer A/90", "Itc", "Int/90", "K Tck", "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", 
             "Yel", "Red"]
 
 # Defensive Midfield Stats
-DM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", "Gls", 
-            "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", 
+DM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", 
+            "Hdr %", "Hdrs", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", 
             "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red", "Dist/90", "CCC", "Ch C/90", 
-            "ShT/90", "Shot/90", "K Pas", "K Pas/90"]
+            "ShT/90", "Shot/90", "K Pas", "K Ps/90"]
 
 # Center Midfield Stats
-CM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst", "Gls", 
-            "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", 
+CM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "Ast", "Fls", "Gl Mst",
+            "Hdr %", "Hdrs", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", "Itc", "Int/90", "K Tck", 
             "Pas %", "Ps C/90", "Tck", "Tck A", "Tck W", "Tck R", "Yel", "Red", "Dist/90", "CCC", "Ch C/90", 
-            "ShT/90", "Shot/90", "K Pas", "K Pas/90"]
+            "ShT/90", "Shot/90", "K Pas", "K Ps/90"]
 
 # Attacking Midfield Stats (Incluing Wingers)
 AM_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "xG", "Gls/90", "Mins/Gl", "Ast", 
-            "Asts/90", "CCC", "Ch C/90", "Cr A", "Cr C", "Cr C/A", "Dist/90", "FA", "K Pas", "K Pas/90", 
+            "Asts/90", "CCC", "Ch C/90", "Cr A", "Cr C", "Cr C/A", "Dist/90", "FA", "K Pas", "K Ps/90", 
             "Pas %", "Pas A", "Ps A/90","Ps C", "Ps C/90", "Shots", "ShT", "ShT/90", "Shot %"]
 
 # Striker Stats
 ST_stats = ["Name", "Age", "Apps", "Av Rat", "Mins", "Mins/Gm", "Gls", "xG", "Gls/90", "Mins/Gl", "Ast", 
             "Asts/90", "CCC", "Ch C/90", "Hdr %", "Hdrs W", "Hdrs A", "Hdrs W/90", "K Hdrs", "Aer A/90", 
-            "Dist/90", "FA", "K Pas", "K Pas/90", "Pas %", "Pas A", "Ps A/90","Ps C", "Ps C/90", "Shots", 
+            "Dist/90", "FA", "K Pas", "K Ps/90", "Pas %", "Pas A", "Ps A/90","Ps C", "Ps C/90", "Shots", 
             "Shot/90", "ShT","ShT/90", "Shot %"]
